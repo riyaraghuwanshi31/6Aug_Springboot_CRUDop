67ef1b7747e4ed68b0bb7a733b86ed5b376b4381
riyaraghuwanshi31 <127579773+riyaraghuwanshi31@users.noreply.github.com>
2024-09-08 21:19:37 +0530

    Update WebConfig.java

--- springboot-restapi/src/main/java/com/springboot/api/springboot_restapi/config/WebConfig.java
+++ springboot-restapi/src/main/java/com/springboot/api/springboot_restapi/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://6-aug-springboot-crudop-8dbr9wgmv-riya-raghuwanshis-projects.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
